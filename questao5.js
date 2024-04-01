41e2d22d8fcf14d1c481a62e563ac667ee41cf86
MetheuAA <matheo200817@gmail.com>
2024-04-01 10:31:00 -0400

    add questao5

---
+++ questao5.js
@@ -0,0 +1,5 @@
+//Questão 5: Como você pode usar uma declaração if, else if, else para verificar se um número é par, ímpar ou zero em JavaScript?
+var numero = 12;
+
+if ( numero %2 === 0) 
+    console.log("É par");
