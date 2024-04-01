e71b3b334bc6092d74583884c93c53f98f3d35d5
MetheuAA <matheo200817@gmail.com>
2024-04-01 10:30:40 -0400

    add questao1

---
+++ questao1.js
@@ -0,0 +1,6 @@
+//Questão 1: Como você pode usar uma declaração if para verificar se uma variável é maior que 10 em JavaScript?
+let idade = 20;
+
+if (idade > 10) {
+    console.log ("Você é menor que 10")
+}
