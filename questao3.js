dba1a3d82a51ea5abf99de4a9d3a9297d5f11b88
MetheuAA <matheo200817@gmail.com>
2024-04-01 10:30:50 -0400

    add questao3

---
+++ questao3.js
@@ -0,0 +1,6 @@
+//Questão 3: Como você pode usar uma declaração if para verificar se uma variável é do tipo string em JavaScript?
+let dia = "sábado";
+
+if (dia === "sábado") {
+  console.log("Hoje é sabado");
+}
