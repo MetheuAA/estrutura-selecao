9984c794aaa7984cf032e79a9a82f0e85e9184f9
MetheuAA <matheo200817@gmail.com>
2024-04-01 10:30:56 -0400

    add questao4

---
+++ questao4.js
@@ -0,0 +1,4 @@
+//Questão 4: Como você pode usar uma declaração if, else if, else para verificar se um número é positivo, negativo ou zero em JavaScript?
+ if ( numero > 0 ) ("O número é positivo.")
+ else if (numero < 0 )("O número é negativo.")
+  else ( numero = 0 )("O número é zero.")
